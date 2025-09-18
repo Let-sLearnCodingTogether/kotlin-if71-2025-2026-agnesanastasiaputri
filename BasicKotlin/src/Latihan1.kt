9e7d1adba31fd5077605511df6ecfb4cfb90735a
Agnes Anastasia Putri <131986066+agnesanastasiaputri@users.noreply.github.com>
2025-09-18 19:58:02 +0700

    Delete BasicKotlin directory

--- BasicKotlin/src/Latihan1.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-    println("2226250013")
-
-    print("Hallo, ")
-
-    print("Agnes Anastasia Putri")
-}
--- BasicKotlin/src/TypeConvertionToInt.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    val myDouble : Double = 99.86
-    val myint : Int = myDouble.toInt()
-
-    println(myint)
-}
