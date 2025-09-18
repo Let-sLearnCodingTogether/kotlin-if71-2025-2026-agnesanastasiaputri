37a04b7cd6741756f7a73a935c89a746d3b44e25
mzackyaulya <muhammadzackyaulya_2226240096@mhs.mdp.ac.id>
2025-09-18 19:55:37 +0700

    Pertemuan1-3

---
+++ BasicKotlin/src/Number.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //Integer Type
+    val age: Byte = 20
+    val weight: Short = 120
+    val distance: Int = 86000
+    val balance: Long = 300_000_000L
+
+    println(age)
+    println(weight)
+    println(distance)
+    println(balance)
+
+    //Unsigned Integer
+    val uAge: UByte = 20u
+    val uWeight: UShort = 120u
+    val uDistance: UInt = 86000u
+    val uBalance: ULong = 4000000u
+
+    //Rapike kode = Ctrl + Alt + L
+    println(uAge)
+
+}
