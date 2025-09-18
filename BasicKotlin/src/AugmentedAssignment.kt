097d070e50dada2fbc06674c82243d962c1dc1e9
mzackyaulya <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2025-09-18 20:00:38 +0700

    Pertemuan 1-3

---
+++ BasicKotlin/src/AugmentedAssignment.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var num: Int = 10
+
+    num += 1 // sama dengan num = num + 1
+    num -= 1 // sama dengan num = num - 1
+    num *= 2 // sama dengan num = num * 2
+    num /= 2 // sama dengan num = num / 2
+    num %= 3 // sama dengan num = num % 3
+
+    println(num)
+
+}
---
+++ BasicKotlin/src/Variable1.kt
@@ -0,0 +1,20 @@
+//const val hrus diluar func
+const val APP_NAME = "Kotlin"
+const val APP_VERSION = "0.0.1"
+
+fun main(){
+    //var msih bs diubah
+    var playerScore = 100
+    println(playerScore)
+
+    playerScore = 103
+    println(playerScore)
+
+    //val dbs diubah lagi
+    val npm = "2226250013"
+    //npm "2226250031"
+    println(npm)
+
+    println(APP_NAME)
+}
+
