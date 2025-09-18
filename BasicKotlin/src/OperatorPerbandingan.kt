26a625b3fa72899ec4633b44fc982dde3f750f66
mzackyaulya <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2025-09-18 20:39:53 +0700

    Pertemuan1-3

---
+++ BasicKotlin/src/OperatorPerbandingan.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val nilaiA = 20
+    val nilaiB = 15
+
+    val apakahLebihBesar = nilaiA > nilaiB
+    println(apakahLebihBesar)
+
+    val apakahSama = nilaiA == nilaiB
+    println(apakahSama)
+
+    val apakahTidakSama = nilaiA != nilaiB
+    println(apakahTidakSama)
+
+    println("apel" < "Kacang") //lihat tabel ascii a = 61, K = 75
+
+}
