6b552870b02c1bd44eac3fbca9ed0a989c356338
Agnes Anastasia Putri <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2025-09-19 19:24:50 +0700

    Pertemuan 3

---
+++ BasicKotlin/src/IfExpression.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val finalExam : Float = 90.88F
+
+    if(finalExam > 90){
+        println("Anda lulus kelas kotlin")
+    }
+    else if(finalExam > 80){
+        println("Mungkin bisa lulus")
+    } else{
+        println("Tidak lulus.")
+    }
+}
