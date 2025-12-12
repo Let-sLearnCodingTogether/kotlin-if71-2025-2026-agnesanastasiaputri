8924e6667d59aa5c30f06f07cd54019cbc79b745
AgnesAnastasiaPutri <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2025-12-12 19:34:59 +0700

    Pertemuan 12 Des 25

---
+++ BasicKotlin/src/Stack/Stack.kt
@@ -0,0 +1,34 @@
+package Stack
+
+interface Stack<Element> {
+    fun push(element: Element)
+    fun pop(): Element?
+}
+
+class StackImplement<T : Any> : Stack<T> {
+    private val storage = arrayListOf<T>()
+
+    override fun toString(): String {
+        return storage.asReversed().toString()
+    }
+
+    override fun push(element: T) {
+        storage.add(element)
+    }
+
+    override fun pop(): T? {
+        if (storage.isEmpty()) return null
+        return storage.removeAt(storage.size - 1)
+    }
+}
+
+fun main() {
+    val stack = StackImplement<Int>()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    println(stack)
+    stack.pop()
+    println(stack)
+}
